bbd95e59e094c5d5cec3c0a0217898517fa69d7d
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-03-01 11:45:36 +0545

    funtion in js

--- Function.js
+++ Function.js
@@ -18,10 +18,50 @@ function printUserDetail(users){
 // printUserDetail(users[0]);
 users.forEach(printUserDetail); */
 
-let courses =["mern","python","qa"]
-function choose(courses){
+/* let courses = ["mern", "python", "qa"]
+function choose(courses) {
     return courses[0];
 }
+console.log(choose(courses)); */
+/* let users=[];
+addNewUser("Rahul",18,"Bkt")
+function addNewUser(name,age,location){
+    let user ={
+        name:name,
+        age:age,
+        location:location
+    }
+    users.push(user);
+}
+console.log(users) */
+
+let students = [
+    {
+        name: "Ramesh",
+        scores: [
+            { exam: "first-term", gpa: 2 },
+            { exam: "second-term", gpa: 4 }
+        ]
+    },
+    {
+        name: "Rahul",
+        scores: [
+            { exam: "first-term", gpa: 1 },
+            { exam: "second-term", gpa: 3 }
+        ]
+    }
+];
+
+function getStudentInfo(students) {
+    return(`${students[0].name} scored GPA ${students[0].scores[0].gpa} in first-term and ${students[0].scores[1].gpa} in second-term.`);
+}
+
+console.log(getStudentInfo(students));
+
+
+
+
+
 
 
 
