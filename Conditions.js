f44440d095a39442b04810346f2a07a9d043f27d
rahulsingh2007 <rahulsingh70815@gmail.com>
2026-03-12 10:24:34 +0545

    Function and Loop

---
+++ Conditions.js
@@ -0,0 +1,40 @@
+/* age =18;
+if (age >= 18) {
+    console.log("You can drive.");
+}
+else {
+    console.log("You can't drive.");
+} */
+
+/* let num = 64;
+if (num % 2 == 0) {
+    console.log("It is even")
+}
+else {
+    console.log("It is odd")
+} */
+
+/* let a = 1;
+let b = 2;
+let c = 3;
+if (a > b && a > c) {
+    console.log("A is greatest")
+} else if (b > a && b > c) {
+    console.log("B is greatest")
+
+} else {
+    console.log("C is greatest")
+} */
+
+/* let age = 16;
+let country = "Nepal";
+if (country =="Nepal" && age>=16){
+    console.log("You can drive")
+}
+else{
+    console.log("You cannot drive")
+} */
+
+/* let age = 14;
+let text = (age < 18) ? "Minor" : "Adult";
+console.log(text) */
---
+++ Loop.js
@@ -0,0 +1,9 @@
+let i=1;
+for (i = 1; i <= 10; i++) {
+    console.log(i)
+}
+let a=1;
+while(a<=5){
+    console.log(a);
+    a++;
+}
